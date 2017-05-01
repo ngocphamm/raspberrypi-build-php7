41a147b35e7ab516c7fad36b1ea92317612e7b6c
Ngoc Pham <ngocphamm@users.noreply.github.com>
2017-05-01 14:30:39 -0400

    Add libicu to the required package needed for PHP7

--- build_php7.sh
+++ build_php7.sh
@@ -6,7 +6,7 @@ VERSION=$1
 
 # Those are needed to build PHP 7 from source
 # sudo apt-get install build-essential
-# sudo apt-get install libfcgi-dev libfcgi0ldbl libjpeg62-turbo-dbg libmcrypt-dev libssl-dev libc-client2007e libc-client2007e-dev libxml2-dev libbz2-dev libcurl4-openssl-dev libjpeg-dev libpng12-dev libfreetype6-dev libkrb5-dev libpq-dev libxml2-dev libxslt1-dev
+# sudo apt-get install libfcgi-dev libfcgi0ldbl libjpeg62-turbo-dbg libmcrypt-dev libssl-dev libc-client2007e libc-client2007e-dev libxml2-dev libbz2-dev libcurl4-openssl-dev libjpeg-dev libpng12-dev libfreetype6-dev libkrb5-dev libpq-dev libxml2-dev libxslt1-dev libicu-dev
 
 BUILD_DIR=/home/pi/Downloads/BuildSrc/php7
 PHP_DIR=/opt/php7
